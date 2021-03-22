f9b200543e67cb9bcb34d96296ea3be4def3716b
Mariano Gonzalez <56511727+marianoGnlz@users.noreply.github.com>
2021-03-22 14:34:06 -0300

    terminado

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+import Formulario from "./components/Formulario";
+import ListadoImagenes from "./components/ListadoImagenes";
 
 function App() {
+  const [busqueda, setBusqueda] = useState("");
+  const [imagenes, setImagenes] = useState([]);
+  const [paginaActual, setPaginaActual] = useState(1);
+  const [totalPaginas, setTotalPaginas] = useState(5);
+
+  useEffect(() => {
+    const consultarApi = async () => {
+      if (busqueda === "") return;
+
+      const imagenesPorPagina = 30;
+      const key = "20807034-598a329be1f2fd78a78a2cb26";
+      const url = `https://pixabay.com/api/?key=${key}&q=${busqueda}
+        &per_page=${imagenesPorPagina}&page=${paginaActual}`;
+
+      const respuesta = await fetch(url);
+      const resultado = await respuesta.json();
+
+      setImagenes(resultado.hits);
+
+      const calcularTotalPaginas = Math.ceil(
+        resultado.totalHits / imagenesPorPagina
+      );
+      setTotalPaginas(calcularTotalPaginas);
+
+      const jumbotron = document.querySelector('.jumbotron');
+      jumbotron.scrollIntoView({behavior: 'smooth'})
+    };
+
+    consultarApi();
+  }, [busqueda, paginaActual]);
+
+  const paginaAnterior = () => {
+    const nuevaPaginaActual = paginaActual - 1;
+    if (nuevaPaginaActual === 0) return;
+    setPaginaActual(nuevaPaginaActual);
+  };
+  const paginaSiguiente = () => {
+    const nuevaPaginaActual = paginaActual + 1;
+    if (nuevaPaginaActual > totalPaginas) return;
+    setPaginaActual(nuevaPaginaActual);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="jumbotron">
+        <p className="lead text-center">Buscador de Imágenes</p>
+        <Formulario setBusqueda={setBusqueda} />
+      </div>
+      <div className="row justify-content-center">
+        <ListadoImagenes imagenes={imagenes} />
+
+        {paginaActual === 1 ? null : (
+          <button
+            type="button"
+            className="btn btn-info mr-1"
+            onClick={paginaAnterior}
+          >
+            &laquo; Anterior
+          </button>
+        )}
+        {paginaActual === totalPaginas ? null : (
+          <button
+            type="button"
+            className="btn btn-info"
+            onClick={paginaSiguiente}
+          >
+            Siguiente &raquo;
+          </button>
+        )}
+      </div>
     </div>
   );
 }
